b85cbec590b362e31b73e99cf9ab30cab8466e47
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-13 17:27:10 +0000

    Fix Pycodestyle whitespace in Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
+
     biggest = my_list[0]
     for num in my_list:
         if num > biggest:
             biggest = num
-            
+
     return biggest
