6ef16be7072eae1311d48137cf8dbfbb2c2dca42
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 09:03:48 +0000

    Fix boolean edge case in size validation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,10 +12,10 @@ class Square:
             size (int): The size of the new square. Defaults to 0.
 
         Raises:
-            TypeError: If size is not an integer.
+            TypeError: If size is not strictly an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
