a1f003363151b7b6b360076f0e6965785cf42aef
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 15:28:58 +0000

    Task 0: Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains the lookup function."""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object.
+    
+    Args:
+        obj: The object to inspect.
+        
+    Returns:
+        list: A list of the object's attributes and methods.
+    """
+    return dir(obj)
