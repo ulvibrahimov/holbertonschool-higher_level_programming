7572a09dd472885bd55bc582f2369018af526ae0
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 09:09:36 +0000

    Revert to standard isinstance and Holberton docstrings

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a class Square with properties to access and update size."""
+"""This module defines a class Square."""
 
 
 class Square:
     """A class that represents a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initialize a new square.
 
         Args:
-            size (int): The size of the new square. Defaults to 0.
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """Retrieve the size of the square."""
+        """Get/set the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
@@ -28,4 +28,4 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return self.__size ** 2
+        return self.__size * self.__size
