74485e4e94068e1bbf789034c46f4b02ccd40456
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 07:14:46 +0000

    Fix pycodestyle W293 spacing errors

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,14 +8,14 @@ It handles basic integer addition and casting from floats.
 def add_integer(a, b=98):
     """
     Adds two integers.
-    
+
     Args:
         a: First number (int or float).
         b: Second number (int or float), defaults to 98.
-        
+
     Returns:
         The addition of a and b as an integer.
-        
+
     Raises:
         TypeError: If a or b is neither an integer nor a float.
     """
@@ -23,5 +23,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-        
+
     return int(a) + int(b)
