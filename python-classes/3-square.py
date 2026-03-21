25953170795db84916473eaefe5bca70a051af2c
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 09:01:10 +0000

    Task 3: Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a class Square with a method to calculate its area."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
