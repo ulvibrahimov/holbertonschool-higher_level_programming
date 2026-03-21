4e2ae97599242de67ddb754ca7b913d9bbcfe20b
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-21 15:31:43 +0000

    Fix pycodestyle trailing whitespace in 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,10 +4,10 @@
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object.
-    
+
     Args:
         obj: The object to inspect.
-        
+
     Returns:
         list: A list of the object's attributes and methods.
     """
