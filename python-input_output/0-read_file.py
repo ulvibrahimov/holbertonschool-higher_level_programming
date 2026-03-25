20b0eb92a83106624baf43cc02ef1da6c8beff50
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-25 09:24:30 +0000

    Task 0: Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that contains a function that reads a text file."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+    
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
