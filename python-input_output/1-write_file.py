0337e9818ef5c392d4239f15407dd3f04c694435
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-25 09:29:58 +0000

    Fix PEP8 line length and whitespace

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,12 +3,14 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written.
-    
+    """Writes a string to a text file (UTF8).
+
+    Returns the number of characters written.
+
     Args:
-        filename (str): The name of the file to write to.
-        text (str): The text to write into the file.
-        
+        filename (str): The name of the file.
+        text (str): The text to write.
+
     Returns:
         int: The number of characters written.
     """
