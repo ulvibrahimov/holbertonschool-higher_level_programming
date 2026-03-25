a9c8e912705ee554b7c1e4dae8986aaaf2eca688
ulvibrahimov <ulvi11112008@gmail.com>
2026-03-25 09:33:18 +0000

    Fix PEP8 line length in module docstring

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Module that contains a function that returns an object from a JSON string."""
+"""Module that returns an object from a JSON string."""
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string.
+    """Returns an object represented by a JSON string.
 
     Args:
         my_str (str): The JSON string to deserialize.
 
     Returns:
-        object: The Python data structure represented by the JSON string.
+        object: The Python data structure from the JSON string.
     """
     return json.loads(my_str)
