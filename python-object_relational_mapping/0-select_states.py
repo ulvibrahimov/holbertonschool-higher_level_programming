d9a349b532d4f6bf04888045b7edecb2d54155b7
ulvibrahimov <ulvi11112008@gmail.com>
2026-04-11 19:58:40 +0000

    Task 0: Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to execute queries
+    cur = db.cursor()
+
+    # Execute the SQL query
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    query_rows = cur.fetchall()
+
+    # Display results
+    for row in query_rows:
+        print(row)
+
+    # Clean up: close cursor and connection
+    cur.close()
+    db.close()
